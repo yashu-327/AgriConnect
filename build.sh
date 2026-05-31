36562c25630058a1dfb6459dda94f431342e5e54
yaswanth <yaswanth63024@gmail.com>
2026-05-31 17:04:37 +0530

    fix render build path

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -o errexit
-
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+cd backend
+
+python manage.py collectstatic --noinput
 
 python manage.py migrate
